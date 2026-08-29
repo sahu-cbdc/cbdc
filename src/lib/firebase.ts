51fdf4121b48a671099cc53c62ee32147674da21
চকবাজার ব্লাড ডোনার'স ক্লাব <chokbazarbloodclub@gmail.com>
2026-08-29 22:11:01 +0600

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -30,7 +30,7 @@ import { getAuth, browserLocalPersistence, setPersistence, type Auth } from "fir
  */
 const DEFAULT_FIREBASE_CONFIG = {
   apiKey: "AIzaSyBxUlGig2NtQLf6tZMRwK6xxzjScNIqbrM",
-  authDomain: "chokbazarbloodclub-69d5f.firebaseapp.com",
+  authDomain: "chawkbazarbloodclub.com",
   databaseURL: "https://chokbazarbloodclub-69d5f-default-rtdb.firebaseio.com",
   projectId: "chokbazarbloodclub-69d5f",
   storageBucket: "chokbazarbloodclub-69d5f.firebasestorage.app",
